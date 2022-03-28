d08a89873fb70159e14d791108b0d178193688c9
Maxime Lemesle <maxime.lemesle7@gmail.com>
2022-03-28 15:50:40 +0200

    Ajout de la page profil

--- templates/profil.php
+++ templates/profil.php
@@ -26,7 +26,7 @@
                 <li><div class="barre"></div></li>
                 <li><a href=""><img class="icone" src="../img/icone/message.svg" alt="Lien vers la page de message"></a></li>
                 <li><div class="barre"></div></li>
-                <li><a href=""><img class="icone" src="../img/icone/profil.svg" alt="Lien vers la page de profil"></a></li>
+                <li><a href="../templates/profil.php"><img class="icone" src="../img/icone/profil.svg" alt="Lien vers la page de profil"></a></li>
             </ul>
             
         </nav>
@@ -39,7 +39,7 @@
 
                 <div class="user">
 
-                    <img classe="pp" src="../img/icone/profil_carre.svg" alt="Photo de profil de user_name">
+                    <img src="../img/icone/profil_carre.svg" alt="Photo de profil de user_name">
 
                     <div class="info">
 
@@ -50,7 +50,7 @@
 
                         <span class="desc">
                             <p class="titre">Description</p>
-                            <p class="text_ita">Hello i’m a fan of NFT and i happy to share my NFT gallery on Neos with you guys</p>
+                            <p class="text_ita">Hello i’m a fan of NFT and i'm happy to share my NFT gallery on Neos with you guys</p>
                         </span>
 
                     </div>
@@ -60,12 +60,12 @@
                         <div class="stat">
 
                             <span class="abonnés">
-                                <p class="titre">Abonnés</p>
+                                <p class="title">Abonnés</p>
                                 <p class="text">1256</p>
                             </span>
 
                             <span class="abonnements">
-                                <p class="titre">Abonnements</p>
+                                <p class="title">Abonnements</p>
                                 <p class="text">125</p>
                             </span>
 
@@ -78,8 +78,8 @@
                             <button class="autre">...</button>
 
                             <span class="report">
-                                <button class="grand">S'abonner</button>
-                                <button class="grand">S'abonner</button>
+                                <button class="grand">Signaler</button>
+                                <button class="grand">Bloquer</button>
                             </span>
 
                         </div>
@@ -92,14 +92,14 @@
 
                     <p class="titre">Collections</p>
 
-                    <img src="../img/nft/CloneX.png" alt="Nft de la collection CloneX" class="nft">
-                    <img src="../img/nft/MekaVerse2.jpg" alt="Nft de la collection Mekaverse" class="nft">
-                    <img src="../img/nft/soulware.jpg" alt="Nft de la collection Soulware" class="nft">
-                    <img src="../img/nft/Unirexes2.jpg" alt="Nft de la collection Unirexes" class="nft">
+                    <div class="nft">
+                        <img src="../img/nft/CloneX.png" alt="Nft de la collection CloneX" class="nft">
+                        <img src="../img/nft/MekaVerse2.jpg" alt="Nft de la collection Mekaverse" class="nft">
+                        <img src="../img/nft/soulware.jpg" alt="Nft de la collection Soulware" class="nft">
+                        <img src="../img/nft/Unirexes2.jpg" alt="Nft de la collection Unirexes" class="nft">
+                    </div>
 
                 </div>
-
-                <div class="fleche"></div>
 
             </div>
 
